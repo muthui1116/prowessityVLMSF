fc64ead639b61d804a59e26d73c603a695db6c72
muthui1116 <muthuikyalozis@gmail.com>
2025-10-26 08:01:24 +0300

    vite api url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
-  withCredentials: true
+  baseURL,
+  withCredentials: true, // send cookies
+  headers: { "Content-Type": "application/json" }
 });
 
 export default client;
